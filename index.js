fd016257c720cef2c67f4cb504ba11795d7c03df
Charles Jolley <charles@sproutit.com>
2010-01-11 19:26:02 -0800

    Make sproutcore wrapper framework CommonJS

---
+++ index.js
@@ -0,0 +1,17 @@
+// ==========================================================================
+// Project:   SproutCore
+// Copyright: ©2006-2010 Sprout Systems, Inc. and contributors.
+//            Portions ©2008-2010 Apple Inc. All rights reserved.
+// License:   Licened under MIT license (see license.js)
+// ==========================================================================
+
+exports = module.exports = require('sproutcore/runtime');
+
+require('sproutcore/datastore');
+
+// can't require these yet as they have not been converted to CommonJS modules
+// they will be made global automatically however so you can assume they are
+// available
+
+//require('sproutcore/foundation');
+//require('sproutcore/desktop');
